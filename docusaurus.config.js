b6c8e1dbf8cb826acc388c6a2b1b5b5f4c7e9b2d
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-10-14 02:14:07 +0800

    remove footer links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,47 +80,6 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
         copyright: `CC-BY-SA 4.0 © ${new Date().getFullYear()} Overdose.wiki Contributors. Built with Docusaurus.`,
       },
       prism: {
