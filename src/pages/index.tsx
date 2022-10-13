56e93aa14da7c932fb791df6e6c44e55d6306070
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-10-14 01:37:19 +0800

    Add logo & Change Link Button Name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,26 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+const Logo: React.ComponentType<React.ComponentProps<'svg'>> = require('@site/static/img/logo.svg').default;
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <Logo className={styles.LogoSvg} role="img" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className={clsx('button button--secondary button--lg', styles.button)}
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            文档
+          </Link>
+          <Link
+            className={clsx('button button--secondary button--lg', styles.button)}
+            to="/blog">
+            博客
           </Link>
         </div>
       </div>
