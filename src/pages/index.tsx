f33ce9f916b09408734e0ae1b797c2700b5e2bc8
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-10-14 00:02:39 +0800

    Update index page title & description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,26 +7,18 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-const Logo: React.ComponentType<React.ComponentProps<'svg'>> = require('@site/static/img/logo.svg').default;
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Logo className={styles.LogoSvg} role="img" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className={clsx('button button--secondary button--lg', styles.button)}
+            className="button button--secondary button--lg"
             to="/docs/intro">
-            文档
-          </Link>
-          <Link
-            className={clsx('button button--secondary button--lg', styles.button)}
-            to="/blog">
-            博客
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
@@ -34,27 +26,14 @@ function HomepageHeader() {
   );
 }
 
-function HomepageIntroduction() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <section className={styles.introduction}>
-      <div className="container">
-        <p>...</p>
-      </div>
-    </section>
-  );
-}
-
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      description="「OverDose.wiki」一个科普和分享药物滥用相关的知识的小网站">
+      description="「OverDose.wiki」一个科普和分享OverDose相关的知识与体验的小网站">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
-        <hr />
-        <HomepageIntroduction />
       </main>
     </Layout>
   );
